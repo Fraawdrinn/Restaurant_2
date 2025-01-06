3b088dc66ffa61d7683bd5f8193da7ce3db4dff1
Fraawdrinn <chatoui.wael40@gmail.com>
2025-01-07 00:53:03 +0100

    added postcss and autoprefixer (and package.json), and finished the loading page

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
-# Restaurant_2
+# Fraawdrinn's seconde website template
+
+## What ?
+
+A frontend project using Tailwind CSS
+
+
+## Why ?
+
+I am practicing using Tailwind for a frontend-only website,
+In order to practice using React.js in future fullstack projects
+
+
+## When ?
+
+Started : 06/01/2025
+Finished : Not finished
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,16 +3,12 @@ const defaultTheme = require('tailwindcss/defaultTheme')
 
 module.exports = {
   content: [
-    "public/**/*.{html, js}",
-    "src/**/*.{js}",
+    "./*.{html, js}",
   ],
   theme: {
     extend: {
+      // Fonts
       'sans': ['PrimarySansFont', ...defaultTheme.fontFamily.sans],
-      'serif': ['CustomSerifFont', ...defaultTheme.fontFamily.serif],
-      'mono': ['CustomMonoFont', ...defaultTheme.fontFamily.mono],
-      'display': ['CustomDisplayFont', 'sans-serif'],
-      
       'montserrat': ['Montserrat', ...defaultTheme.fontFamily.sans],
       'poppins': ['Poppins', ...defaultTheme.fontFamily.sans],
     },
